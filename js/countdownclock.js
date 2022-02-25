cc247fd16b3177feca70e06de52b0391ce0d8c52
semdial2022 <semdial2022@gmail.com>
2022-02-25 15:59:59 +0000

    Changing it to more 2022 again

--- js/countdownclock.js
+++ js/countdownclock.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date('September 4, 2019 09:00:00');
+var deadline = new Date('August 22, 2022 09:00:00');
 initializeClock('clockdiv', deadline);
